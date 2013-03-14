1d04eeb4d9c0683e720858b19741b9edb7d81a5d
Alan Gutierrez <alan@prettyrobots.com>
2013-03-14 07:26:19 -0400

    Test `validator`.
    Created a test directory called `coverage` where I'm going to put tests
    that were proscribed by Istanbul. I may organize them into different
    directories later on.

---
+++ t/coverage/validator.t.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+require("./proof")(2, function (Strata, equal, ok, tmp) {
+
+  strata = new Strata(__filename, {}) ;
+
+  strata.create(function (error) {
+    ok(/is not a directory.$/.test(error.message), 'thrown');
+  });
+
+  strata = new Strata(tmp, {  fs: {
+    stat: function (file, callback) { callback(new Error('errored')) }
+  }});
+
+  strata.create(function (error) {
+    equal(error.message, 'errored', 'called back');
+  });
+});
