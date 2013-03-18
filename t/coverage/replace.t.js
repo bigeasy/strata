8672fcf74f902dfbb31f9a523086eb9a17ab5441
Alan Gutierrez <alan@prettyrobots.com>
2013-03-18 01:02:11 -0400

    Test error on replacement unlink.

---
+++ t/coverage/replace.t.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+require("./proof")(1, function (step, Strata, deepEqual, tmp, insert, load, objectify, equal) {
+
+  function forward (name) { return function () { return fs[name].apply(fs, arguments) } }
+
+  
+  var fs = require('fs'), path = require('path'), proxy = {};
+  for (var x in fs) {
+    if (x[0] != '_') proxy[x] = forward(x);
+  }
+  proxy.unlink = function (file, callback) {
+    var error = new Error();
+    error.code = "EACCES";
+    callback(error);
+  }
+  var strata = new Strata(tmp, { fs: proxy, leafSize: 3 });
+
+  step(function (serialize) {
+    serialize(__dirname + "/../basics/fixtures/split.before.json", tmp, step());
+  }, function () {
+    strata.open(step());
+  }, function () {
+    insert(step, strata, [ "b" ]);
+  }, function () {
+    strata.balance(step());
+  }, function (error) {
+    equal(error.code, "EACCES", "unlink error");
+  });
+});
