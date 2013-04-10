9b023618be75a09bfda958dc4e053d89a83686bc
Alan Gutierrez <alan@prettyrobots.com>
2013-04-09 23:11:16 -0400

    Test purge when a page is locked.

---
+++ t/lock/purge-skip.t.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+require("./proof")(2, function (step, equal, Strata, tmp, serialize) {
+  var strata;
+
+  step(function () {
+
+    serialize(__dirname + '/fixtures/tree.before.json', tmp, step());
+
+  }, function () {
+
+    strata = new Strata(tmp, { leafSize: 3, branchSize: 3 });
+    strata.open(step());
+
+  }, function () {
+
+    strata.mutator('h', step());
+
+  }, function (cursor) {
+
+    equal(strata.size, 130, 'before purge');
+    strata.purge(0);
+    equal(strata.size, 32, 'after purge');
+    cursor.unlock();
+
+  }, function() {
+
+    strata.close(step());
+
+  });
+});
+
