dcfcad2d53851e3a1cddc63903e7d56a5a8c0279
Alan Gutierrez <alan@prettyrobots.com>
2013-06-17 00:23:20 -0400

    Test propagated splits.
    Test spliting a branch page into many branch pages. Test splitting the
    root page into many branch pages and then again splitting the root page
    when it is still larger than the branch page size.
    
    Closes #85.

---
+++ t/split/propagate.t.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+require('./proof')(1, function (step, Strata, tmp, deepEqual, serialize, gather, load, objectify) {
+  var strata = new Strata(tmp, { leafSize: 3, branchSize: 3 }), fs = require('fs');
+  step(function () { 
+    serialize(__dirname + '/fixtures/propagate.before.json', tmp, step());
+  }, function () {
+    strata.open(step());
+  }, function () {
+    strata.mutator('zz', step());
+  }, function (cursor) {
+    step(function () {
+      cursor.insert('zz', 'zz', ~ cursor.index, step());
+    }, function () {
+      cursor.unlock()
+    });
+  }, function () {
+    strata.balance(step());
+  }, function () {
+    objectify(tmp, step());
+    load(__dirname + '/fixtures/propagate.after.json', step());
+  }, function (actual, expected) {
+    deepEqual(actual, expected, 'split');
+    strata.close(step());
+  });
+});
