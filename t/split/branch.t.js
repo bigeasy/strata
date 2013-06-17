c8a53633c15e281a1630de2c7b2696aab55d60de
Alan Gutierrez <alan@prettyrobots.com>
2013-06-17 10:09:22 -0400

    Tidy up names of fixtures.

--- t/split/branch.t.js
+++ t/split/branch.t.js
@@ -3,7 +3,7 @@
 require('./proof')(3, function (step, Strata, tmp, deepEqual, serialize, gather, load, objectify) {
   var strata = new Strata(tmp, { leafSize: 3, branchSize: 3 }), fs = require('fs');
   step(function () { 
-    serialize(__dirname + '/fixtures/branch-split.before.json', tmp, step());
+    serialize(__dirname + '/fixtures/branch.before.json', tmp, step());
   }, function () {
     strata.open(step());
   }, function () {
@@ -21,7 +21,7 @@ require('./proof')(3, function (step, Strata, tmp, deepEqual, serialize, gather,
     strata.balance(step());
   }, function () {
     objectify(tmp, step());
-    load(__dirname + '/fixtures/branch-split.after.json', step());
+    load(__dirname + '/fixtures/branch.after.json', step());
   }, function (actual, expected) {
     deepEqual(actual, expected, 'split');
 
