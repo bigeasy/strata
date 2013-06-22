14de21930f58c1ae7a8f9084dbbfa728feae6ac1
Alan Gutierrez <alan@prettyrobots.com>
2013-06-22 00:40:40 -0400

    Test split leaf at capacity with a ghost.
    Test that such a leaf page does not actually split.
    
    Closes #21.

---
+++ t/ghost/unsplittable.t.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+require('./proof')(3, function (step, Strata, tmp, deepEqual, serialize, gather, load, objectify) {
+  var strata = new Strata(tmp, { leafSize: 3, branchSize: 3 }), fs = require('fs');
+  step(function () {
+    serialize(__dirname + '/fixtures/unsplittable.before.json', tmp, step());
+  }, function () {
+    strata.open(step());
+  }, function () {
+    strata.mutator('g', step());
+  }, function (cursor) {
+    step(function () {
+      cursor.insert('g', 'g', ~cursor.index, step());
+    }, function () {
+      cursor.indexOf('d', step());
+    }, function (index) {
+      cursor.remove(index, step());
+    }, function () {
+      cursor.unlock();
+      gather(step, strata);
+    });
+  }, function (records) {
+    deepEqual(records, [ 'a', 'b', 'c', 'e', 'f', 'g' ], 'records');
+    strata.balance(step());
+  }, function () {
+    gather(step, strata);
+  }, function (records) {
+    deepEqual(records, [ 'a', 'b', 'c', 'e', 'f', 'g' ], 'balanced');
+    objectify(tmp, step());
+    load(__dirname + '/fixtures/unsplittable.after.json', step());
+  }, function (actual, expected) {
+    deepEqual(actual, expected, 'after');
+    strata.close(step());
+  });
+});
