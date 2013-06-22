3b3cf905d83b8959b974a5418f7642253dc16273
Alan Gutierrez <alan@prettyrobots.com>
2013-06-22 15:44:26 -0400

    Test that ghosts are not left in first leaf page.
    The first leaf page is always the least page in the tree where records
    whose key is less than the least key are inserted. There is no need to
    maintain a ghost key because the first record is never used as a branch
    page key.
    
    Closes #102.

---
+++ t/ghost/first.t.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+require('./proof')(2, function (step, Strata, tmp, deepEqual, serialize, load, objectify) {
+  var strata = new Strata(tmp, { leafSize: 3, branchSize: 3 }), fs = require('fs');
+  step(function () {
+    serialize(__dirname + '/fixtures/first.before.json', tmp, step());
+  }, function () {
+    strata.open(step());
+  }, function () {
+    strata.mutator('a', step());
+  }, function (cursor) {
+    step(function () {
+      cursor.remove(cursor.index, step());
+    }, function () {
+      cursor.unlock();
+    });
+  }, function () {
+    objectify(tmp, step());
+    load(__dirname + '/fixtures/first.after.json', step());
+  }, function (actual, expected) {
+    deepEqual(actual, expected, 'after');
+    strata.vivify(step());
+  }, function (result) {
+    deepEqual(result, [ { address: -1, children: [ 'b', 'c' ], ghosts: 0 } ], 'ghostbusters');
+  }, function () {
+    strata.close(step());
+  });
+});
