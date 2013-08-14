1da1d4f5dce730727827311c30ffa8a341c5dbb8
Alan Gutierrez <alan@prettyrobots.com>
2013-08-14 01:55:37 +0000

    Test no left sibling in `chooseBranchesToMerge`.
    Same test as unpurged key, which covered the uncovered condition of
    determining how to merge a branch page that has no left sibling.
    
    Closes #150.

---
+++ t/merge/merge-no-left-branch.t.js
@@ -0,0 +1,12 @@
+// Same test as unpurged key, which covered the uncovered condition of
+// determining how to merge a branch page that has no left sibliing.
+
+require('./proof')(1, function (step, tmp, cadence, script, deepEqual) {
+  var path = require('path');
+  script({
+    file: path.join(__dirname, 'fixtures', 'unpurged-key.txt'),
+    directory: tmp,
+    cadence: cadence,
+    deepEqual: deepEqual
+  }, step());
+})
