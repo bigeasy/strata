195d52fc9a8827b7da0b7bbb9723ab7a10931a8b
Alan Gutierrez <alan@prettyrobots.com>
2013-11-03 19:32:24 +0000

    Test specifying checksum via constructor.
    Closes #255.

---
+++ t/coverage/checksum-constructor.t.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+require('./proof')(1, function (Strata, equal, deepEqual, say, tmp, step, ok, load, objectify) {
+    var fs = require('fs'), crypto = require('crypto'), strata
+    step(function () {
+        fs.writeFile(tmp + '/.ignore', '', 'utf8', step())
+    }, function () {
+        strata = new Strata({ directory: tmp, leafSize: 3, branchSize: 3 })
+        strata.create(step())
+    }, function () {
+        strata.close(step())
+    }, function () {
+        strata = new Strata({ directory: tmp, leafSize: 3, branchSize: 3, checksum: function () { return crypto.createHash('sha1') } })
+        strata.open(step())
+    }, function () {
+        strata.iterator('a', step())
+    }, function (cursor) {
+        equal(cursor.length - cursor.offset, 0, 'empty')
+        cursor.unlock()
+        strata.close(step())
+    })
+})
