23a7d8dd313c3dfca61af1b96a181a8473db54c2
Alan Gutierrez <alan@prettyrobots.com>
2014-03-29 23:53:17 -0700

    Tidy `t/basics/get.t.js`.
    See #351.

--- t/basics/get.t.js
+++ t/basics/get.t.js
@@ -1,34 +1,22 @@
 #!/usr/bin/env node
 
-var fs = require('fs'), strata
-require('./proof')(9, function (serialize, tmp, equal, step, Strata, ok) {
+require('./proof')(9, function (step, Strata, tmp, serialize, equal, ok) {
+    var strata
     step(function () {
-
         serialize(__dirname + '/fixtures/get.json', tmp, step())
-
     }, function () {
-
         strata = new Strata({ directory: tmp, leafSize: 3, branchSize: 3 })
         strata.open(step())
-
     }, function () {
-
         equal(strata.size, 0, 'json size before read')
-
         strata.iterator('a', step())
-
     }, function (cursor) {
-
         step(function () {
-
             ok(! cursor.exclusive, 'shared')
             equal(cursor.index, 0, 'index')
             equal(cursor.offset, 0, 'offset')
-
             cursor.get(cursor.offset, step())
-
         }, function (record, key, size) {
-
             equal(record, 'a', 'get record')
             equal(key, 'a', 'get key')
             equal(strata.size, 14, 'json size after read')
