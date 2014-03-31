4a97c94c9fab1c5769d3c535db7ccc829b29941d
Alan Gutierrez <alan@prettyrobots.com>
2014-03-30 23:00:37 -0700

    Use `assert` in `t/basics/delete.t.js`.
    See #351.

--- t/basics/get.t.js
+++ t/basics/get.t.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-require('./proof')(9, function (step, Strata, tmp, serialize, equal, ok) {
+require('./proof')(9, function (step, Strata, tmp, serialize, assert) {
     var strata
     step(function () {
         serialize(__dirname + '/fixtures/get.json', tmp, step())
@@ -8,24 +8,24 @@ require('./proof')(9, function (step, Strata, tmp, serialize, equal, ok) {
         strata = new Strata({ directory: tmp, leafSize: 3, branchSize: 3 })
         strata.open(step())
     }, function () {
-        equal(strata.size, 0, 'json size before read')
+        assert(strata.size, 0, 'json size before read')
         strata.iterator('a', step())
     }, function (cursor) {
         step(function () {
-            ok(! cursor.exclusive, 'shared')
-            equal(cursor.index, 0, 'index')
-            equal(cursor.offset, 0, 'offset')
+            assert(! cursor.exclusive, 'shared')
+            assert(cursor.index, 0, 'index')
+            assert(cursor.offset, 0, 'offset')
             cursor.get(cursor.offset, step())
         }, function (record, key, size) {
-            equal(record, 'a', 'get record')
-            equal(key, 'a', 'get key')
-            equal(strata.size, 14, 'json size after read')
-            equal(size, 54, 'record size')
+            assert(record, 'a', 'get record')
+            assert(key, 'a', 'get key')
+            assert(strata.size, 14, 'json size after read')
+            assert(size, 54, 'record size')
 
             cursor.unlock()
 
             strata.purge(0)
-            equal(strata.size, 0, 'page')
+            assert(strata.size, 0, 'page')
 
             strata.close(step())
         })
