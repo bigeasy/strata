de8027c501cdcc515c900f5d70c34a8fd68b17c3
Alan Gutierrez <alan@prettyrobots.com>
2014-03-30 23:13:03 -0700

    Tidy `t/basics/insert.t.js`.
    See #351.

--- t/basics/insert.t.js
+++ t/basics/insert.t.js
@@ -1,42 +1,25 @@
 #!/usr/bin/env node
 
-require('./proof')(5, function (step, tmp, Strata, ok, equal, load, objectify, say, deepEqual) {
-    var fs = require('fs'), strata
-
+require('./proof')(5, function (step, Strata, tmp, load, objectify, ok, equal, deepEqual, say) {
+    var strata
     step(function () {
-
         strata = new Strata({ directory: tmp, leafSize: 3, branchSize: 3 })
         strata.create(step())
-
     }, function () {
-
         strata.mutator('a', step())
-
     }, function (cursor) {
-
         ok(cursor.exclusive, 'exclusive')
-
         step(function () {
-
             cursor.insert('a', 'a', ~ cursor.index, step())
-
         }, function (inserted) {
-
             equal(inserted, 0, 'inserted')
-
             cursor.unlock()
-
             equal(strata.size, 14, 'json size')
-
         })
-
     }, function () {
-
         objectify(tmp, step())
         load(__dirname + '/fixtures/insert.json', step())
-
     }, function (actual, expected) {
-
         say(expected)
         say(actual)
 
