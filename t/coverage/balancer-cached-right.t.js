d0b9574d4c90d51b8808217cc48bcb0c2ea16a97
Alan Gutierrez <alan@prettyrobots.com>
2014-03-31 23:17:46 -0700

    Tidy `t/coverage/balancer-cached-right.t.js`.
    See #351.

--- t/coverage/balancer-cached-right.t.js
+++ t/coverage/balancer-cached-right.t.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-require('./proof')(2, function (step, Strata, tmp, load, serialize, objectify, gather, assert) {
+require('./proof')(2, function (step, Strata, tmp, serialize, gather, assert) {
     var strata = new Strata({ directory: tmp, leafSize: 3, branchSize: 3 })
     step(function () {
         serialize(__dirname + '/fixtures/balancer-cached-right.json', tmp, step())
