b1117fbc2efc98926f0e2359a3645224bcdbdcc1
Alan Gutierrez <alan@prettyrobots.com>
2014-04-01 23:31:26 -0700

    Use `vivify` in `checksum-constructor.t.js`.
    See #352.

--- t/coverage/large-leaf.t.js
+++ t/coverage/large-leaf.t.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-require('./proof')(2, function (step, Strata, tmp, load, serialize, objectify, gather, assert) {
+require('./proof')(2, function (step, Strata, tmp, load, serialize, vivify, gather, assert) {
     var strata = new Strata({ directory: tmp, leafSize: 3, branchSize: 3 })
     step(function () {
         serialize(__dirname + '/fixtures/large-leaf.before.json', tmp, step())
@@ -20,7 +20,7 @@ require('./proof')(2, function (step, Strata, tmp, load, serialize, objectify, g
         assert(records, '_abcdefghijklmnopqrstuvwxyz'.split(''), 'records')
         strata.balance(step())
     }, function () {
-        objectify(tmp, step())
+        vivify(tmp, step())
         load(__dirname + '/fixtures/large-leaf.after.json', step())
     }, function (actual, expected) {
         assert(actual, expected, 'split')
