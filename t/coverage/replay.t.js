43a3e82145b980e85e18372a3f50fe3d00cd2689
Alan Gutierrez <alan@prettyrobots.com>
2014-04-01 23:56:02 -0700

    Tidy `t/coverage/replay.t.js`.
    See #351.

--- t/coverage/replay.t.js
+++ t/coverage/replay.t.js
@@ -2,8 +2,8 @@
 
 // Asserts that log replay will skip over the positions array.
 
-require('./proof')(1, function (step, tmp, Strata, deepEqual, say, gather) {
-    var fs = require('fs'), strata
+require('./proof')(1, function (step, Strata, tmp, gather, deepEqual, say) {
+    var strata
     step(function () {
         strata = new Strata({ directory: tmp, leafSize: 3, branchSize: 3 })
         strata.create(step())
