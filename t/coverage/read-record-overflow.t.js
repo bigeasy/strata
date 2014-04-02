eb1db2eef56b9848648fbd91337db801a86bc244
Alan Gutierrez <alan@prettyrobots.com>
2014-04-01 23:42:45 -0700

    Tidy `read-record-overflow.t.js`.
    See #351.

--- t/coverage/read-record-overflow.t.js
+++ t/coverage/read-record-overflow.t.js
@@ -1,8 +1,12 @@
 #!/usr/bin/env node
 
-require('./proof')(1, function (step, Strata, deepEqual, tmp, gather, serialize) {
-    var strata = new Strata({ directory: tmp, branchSize: 3, leafSize: 3, readRecordStartLength: 2 })
-
+require('./proof')(1, function (step, Strata, tmp, serialize, gather, deepEqual) {
+    var strata = new Strata({
+        directory: tmp,
+        branchSize: 3,
+        leafSize: 3,
+        readRecordStartLength: 2
+    })
     step(function () {
         serialize(__dirname + '/fixtures/read-record.before.json', tmp, step())
     }, function () {
