c10427dfb28977f230850b3973a1915853d422fb
Alan Gutierrez <alan@prettyrobots.com>
2014-04-04 16:03:01 -0700

    Tidy `t/coverage/set-immediate.t.js`.
    See #351.

--- t/coverage/set-immediate.t.js
+++ t/coverage/set-immediate.t.js
@@ -1,33 +1,25 @@
 #!/usr/bin/env node
 
-require('./proof')(1, function (serialize, tmp, equal, step, Strata, ok) {
-    var fs = require('fs'), strata
-
+require('./proof')(1, function (step, Strata, tmp, serialize, equal, ok) {
+    var strata
     step(function () {
-
         serialize(__dirname + '/../basics/fixtures/get.json', tmp, step())
-
     }, function () {
-
-        strata = new Strata({ directory: tmp, leafSize: 3, branchSize: 3, setImmediate: true })
+        strata = new Strata({
+            directory: tmp,
+            leafSize: 3,
+            branchSize: 3,
+            setImmediate: true
+        })
         strata.open(step())
-
     }, function () {
-
         strata.iterator('a', step())
-
     }, function (cursor) {
-
         step(function () {
-
             cursor.get(cursor.offset, step())
-
         }, function (got) {
-
             equal(got, 'a', 'get')
-
             cursor.unlock()
-
             strata.close(step())
         })
     })
