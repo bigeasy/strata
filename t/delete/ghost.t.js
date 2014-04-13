2ffe3c1ea2f3b2c9e7b3d5fd780a3bdd326bb650
Alan Gutierrez <alan@prettyrobots.com>
2014-04-12 21:48:53 -0700

    Use `assert` in `t/delete/ghost.t.js`.
    See #351.

--- t/delete/ghost.t.js
+++ t/delete/ghost.t.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-require('./proof')(5, function (step, Strata, tmp, load, serialize, objectify, gather, equal, deepEqual, say) {
+require('./proof')(5, function (step, Strata, tmp, load, serialize, objectify, gather, assert, say) {
     var strata = new Strata({ directory: tmp, leafSize: 3, branchSize: 3 })
     step(function () {
         serialize(__dirname + '/fixtures/delete.json', tmp, step())
@@ -9,7 +9,7 @@ require('./proof')(5, function (step, Strata, tmp, load, serialize, objectify, g
     }, function () {
         gather(step, strata)
     }, function (records) {
-        deepEqual(records, [ 'a', 'b', 'c', 'd' ], 'records')
+        assert(records, [ 'a', 'b', 'c', 'd' ], 'records')
     }, function () {
         strata.mutator('c', step())
     }, function (cursor) {
@@ -22,13 +22,13 @@ require('./proof')(5, function (step, Strata, tmp, load, serialize, objectify, g
             gather(step, strata)
         })
     }, function (records) {
-        deepEqual(records, [ 'a', 'b', 'd' ], 'deleted')
+        assert(records, [ 'a', 'b', 'd' ], 'deleted')
         objectify(tmp, step())
         load(__dirname + '/fixtures/ghost.after.json', step())
     }, function (actual, expected) {
         say(expected)
         say(actual)
-        deepEqual(actual, expected, 'directory')
+        assert(actual, expected, 'directory')
         strata.close(step())
     }, function () {
         strata = new Strata({ directory: tmp, leafSize: 3, branchSize: 3 })
@@ -39,13 +39,13 @@ require('./proof')(5, function (step, Strata, tmp, load, serialize, objectify, g
         step(function () {
             cursor.next(step())
         }, function (next) {
-            equal(cursor.offset, 1, 'ghosted')
+            assert(cursor.offset, 1, 'ghosted')
             cursor.unlock()
         })
     }, function () {
         gather(step, strata)
     }, function (records) {
-        deepEqual(records, [ 'a', 'b', 'd' ], 'reopened')
+        assert(records, [ 'a', 'b', 'd' ], 'reopened')
         strata.close(step())
     })
 })
