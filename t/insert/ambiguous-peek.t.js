00c491f432c4abc68daa6d3dec3b7e2f70c12f26
Alan Gutierrez <alan@prettyrobots.com>
2014-04-13 22:58:03 -0700

    Use `assert` in `ambiguous-peek.t.js`.
    See #351.

--- t/insert/ambiguous-peek.t.js
+++ t/insert/ambiguous-peek.t.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-require('./proof')(4, function (step, Strata, tmp, serialize, gather, deepEqual, equal) {
+require('./proof')(4, function (step, Strata, tmp, serialize, gather, assert) {
     var strata = new Strata({ directory: tmp, leafSize: 3, branchSize: 3 })
     step(function () {
         serialize(__dirname + '/fixtures/ambiguous.before.json', tmp, step())
@@ -9,7 +9,7 @@ require('./proof')(4, function (step, Strata, tmp, serialize, gather, deepEqual,
     }, function () {
         gather(step, strata)
     }, function (records) {
-        deepEqual(records, [ 'a', 'd', 'f', 'g', 'h', 'i', 'l', 'm', 'n' ], 'records')
+        assert(records, [ 'a', 'd', 'f', 'g', 'h', 'i', 'l', 'm', 'n' ], 'records')
     }, function () {
         strata.mutator('a', step())
     }, function (cursor) {
@@ -18,18 +18,18 @@ require('./proof')(4, function (step, Strata, tmp, serialize, gather, deepEqual,
         }, function (index) {
             cursor.insert('b', 'b', ~index, step())
         }, function (unambiguous) {
-            equal(unambiguous, 0, 'unambiguous')
+            assert(unambiguous, 0, 'unambiguous')
             cursor.indexOf('c', step())
         }, function (index) {
             cursor.insert('c', 'c', ~index, step())
         }, function (unambiguous) {
             cursor.unlock()
-            equal(unambiguous, 0, 'unambiguous cached')
+            assert(unambiguous, 0, 'unambiguous cached')
         }, function () {
             gather(step, strata)
         })
     }, function (records) {
-        deepEqual(records, [ 'a', 'b', 'c', 'd', 'f', 'g', 'h', 'i', 'l', 'm', 'n' ], 'records')
+        assert(records, [ 'a', 'b', 'c', 'd', 'f', 'g', 'h', 'i', 'l', 'm', 'n' ], 'records')
     }, function() {
         strata.close(step())
     })
