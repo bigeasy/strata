311eab66a8c599d32237fdd1b15af91b47bd14e9
Demarius Chrite <chrite.demarius@gmail.com>
2014-04-13 18:32:50 -0700

    Use `assert` in `ambiguous-exact-ghosts.t.js`.
    See #351.

--- t/insert/ambiguous-exact-ghosts.t.js
+++ t/insert/ambiguous-exact-ghosts.t.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-require('./proof')(4, function (step, Strata, tmp, serialize, gather, deepEqual, equal) {
+require('./proof')(4, function (step, Strata, tmp, serialize, gather, assert) {
     var strata = new Strata({ directory: tmp, leafSize: 3, branchSize: 3 })
     step(function () {
         serialize(__dirname + '/fixtures/ambiguous.before.json', tmp, step())
@@ -9,7 +9,7 @@ require('./proof')(4, function (step, Strata, tmp, serialize, gather, deepEqual,
     }, function () {
         gather(step, strata)
     }, function (records) {
-        deepEqual(records, [ 'a', 'd', 'f', 'g', 'h', 'i', 'l', 'm', 'n' ], 'records')
+        assert(records, [ 'a', 'd', 'f', 'g', 'h', 'i', 'l', 'm', 'n' ], 'records')
     }, function () {
         strata.mutator('g', step())
     }, function (cursor) {
@@ -20,19 +20,19 @@ require('./proof')(4, function (step, Strata, tmp, serialize, gather, deepEqual,
             gather(step, strata)
         })
     }, function (records) {
-        deepEqual(records, [ 'a', 'd', 'f', 'h', 'i', 'l', 'm', 'n' ], 'records after delete')
+        assert(records, [ 'a', 'd', 'f', 'h', 'i', 'l', 'm', 'n' ], 'records after delete')
         strata.mutator('j', step())
     }, function (cursor) {
         step(function () {
             cursor.insert('j', 'j', ~cursor.index, step())
         }, function (unambiguous) {
-            equal(unambiguous, 0, 'unambiguous')
+            assert(unambiguous, 0, 'unambiguous')
             cursor.unlock()
         }, function () {
             gather(step, strata)
         })
     }, function (records) {
-        deepEqual(records, [ 'a', 'd', 'f', 'h', 'i', 'j', 'l', 'm', 'n' ], 'records after insert')
+        assert(records, [ 'a', 'd', 'f', 'h', 'i', 'j', 'l', 'm', 'n' ], 'records after insert')
     }, function() {
         strata.close(step())
     })
