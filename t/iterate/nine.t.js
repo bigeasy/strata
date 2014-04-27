a09503f4f3ff86ce531c806a492a7832be16be0e
Demarius Chrite <chrite.demarius@gmail.com>
2014-04-26 20:40:42 -0700

    Tidy `t/iterate/nine.t.js`.
    See #351.

--- t/iterate/nine.t.js
+++ t/iterate/nine.t.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-require('./proof')(4, function (step, Strata, tmp, deepEqual, serialize, equal, gather) {
+require('./proof')(4, function (step, Strata, tmp, serialize, gather, equal, deepEqual) {
     var strata = new Strata({ directory: tmp, leafSize: 3, branchSize: 3 })
     step(function () {
         serialize(__dirname + '/fixtures/nine.json', tmp, step())
