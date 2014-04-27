19fea8c7fa0f4de1193fc5cc746079a24953eac9
Alan Gutierrez <alan@prettyrobots.com>
2014-04-27 08:27:06 -0700

    Tidy `t/iterate/one.t.js`.
    See #351.

--- t/iterate/one.t.js
+++ t/iterate/one.t.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 require('./proof')(5, function (step, Strata, tmp, serialize, equal) {
-    var strata = new Strata({ directory: tmp, leafSize: 3, branchSize: 3 }), fs = require('fs'), records = []
+    var strata = new Strata({ directory: tmp, leafSize: 3, branchSize: 3 })
     step(function () {
         serialize(__dirname + '/fixtures/one.json', tmp, step())
     }, function () {
