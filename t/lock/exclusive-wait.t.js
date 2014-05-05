baaca7911375fb319b6b00a583e1dfddd14beb99
Alan Gutierrez <alan@prettyrobots.com>
2014-05-04 21:40:02 -0700

    Tidy `exclusive-wait.t.js`.
    See #351.

--- t/lock/exclusive-wait.t.js
+++ t/lock/exclusive-wait.t.js
@@ -1,40 +1,26 @@
 #!/usr/bin/env node
 
-require('./proof')(1, function (step, ok, equal, Strata, tmp, deepEqual,
-    say, die, serialize, gather, load, objectify) {
-    var strata, cursor, purge, count = 0
-
+require('./proof')(1, function (step, Strata, tmp, serialize, ok) {
+    var strata
     step(function () {
-
         serialize(__dirname + '/fixtures/tree.before.json', tmp, step())
-
     }, function () {
-
         strata = new Strata({ directory: tmp, leafSize: 3, branchSize: 3 })
         strata.open(step())
-
     }, function () {
-
         strata.iterator('h', step())
-
     }, function (cursor) {
-
         strata.mutator('h', step())
         cursor.unlock()
-
     }, function (cursor) {
-
         step(function () {
             cursor.get(cursor.offset, step())
         }, function (value) {
             ok(value, 'h', 'got')
             cursor.unlock()
         })
-
     }, function() {
-
         strata.close(step())
-
     })
 })
 
