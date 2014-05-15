11cd6f844300bc200862c61abf6bfb4fb4191069
Alan Gutierrez <alan@prettyrobots.com>
2014-05-14 22:04:15 -0700

    Tidy `t/lock/loading.t.js`.
    See #351.

--- t/lock/loading.t.js
+++ t/lock/loading.t.js
@@ -1,43 +1,30 @@
 #!/usr/bin/env node
 
-require('./proof')(2, function (step, ok, equal, Strata, tmp, deepEqual,
-    say, die, serialize, gather, load, objectify) {
-    var strata, cursor, purge, count = 0
-
+require('./proof')(2, function (step, Strata, tmp, serialize, ok) {
+    var strata
     step(function () {
-
         serialize(__dirname + '/fixtures/tree.before.json', tmp, step())
-
     }, function () {
-
         strata = new Strata({ directory: tmp, leafSize: 3, branchSize: 3 })
         strata.open(step())
-
     }, function () {
-
         strata.iterator('h', step())
         strata.iterator('h', step())
-
     }, function (first, second) {
-
         step(function () {
             first.get(first.offset, step())
         }, function (value) {
             ok(value, 'h', 'first')
             first.unlock()
         })
-
         step(function () {
             second.get(second.offset, step())
         }, function (value) {
             ok(value, 'h', 'second')
             second.unlock()
         })
-
     }, function() {
-
         strata.close(step())
-
     })
 })
 
