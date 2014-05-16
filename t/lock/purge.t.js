ff3bc7c649f5b5d1f59977281ab15bec318e18f8
Alan Gutierrez <alan@prettyrobots.com>
2014-05-15 21:52:20 -0700

    Tidy `t/lock/purge.t.js`.
    See #351.

--- t/lock/purge.t.js
+++ t/lock/purge.t.js
@@ -1,8 +1,7 @@
 #!/usr/bin/env node
 
-require('./proof')(4, function (step, ok, equal, Strata, tmp, deepEqual,
-    say, serialize, gather, load, objectify) {
-    var strata, purge, count = 0
+require('./proof')(4, function (step, Strata, tmp, serialize, load, objectify, gather, ok, equal, deepEqual) {
+    var strata, count = 0
 
     function tracer (type, object, callback) {
         switch (type) {
@@ -20,89 +19,48 @@ require('./proof')(4, function (step, ok, equal, Strata, tmp, deepEqual,
     }
 
     step(function () {
-
         serialize(__dirname + '/fixtures/tree.before.json', tmp, step())
-
     }, function () {
-
         strata = new Strata({ directory: tmp, leafSize: 3, branchSize: 3, tracer: tracer })
         strata.open(step())
-
     }, function () {
-
         strata.mutator('h', step())
-
     }, function (cursor) {
-
         step(function () {
-
             cursor.indexOf('h', step())
-
         }, function (index) {
-
             cursor.remove(index, step())
-
         }, function () {
-
             cursor.indexOf('i', step())
-
         }, function (index) {
-
             cursor.remove(index, step())
-
         }, function () {
-
             cursor.unlock()
-
         })
     }, function () {
-
         strata.mutator('e', step())
-
     }, function (cursor) {
-
         step(function () {
-
             cursor.indexOf('e', step())
-
         }, function (index) {
-
             cursor.remove(index, step())
-
         }, function () {
-
             cursor.indexOf('g', step())
-
         }, function (index) {
-
             cursor.remove(index, step())
-
         }, function () {
-
             cursor.unlock()
-
         })
     }, function () {
-
         gather(step, strata)
-
     }, function (records) {
-
         deepEqual(records, [ 'a', 'b', 'c', 'd',  'f', 'j', 'k', 'l', 'm', 'n' ], 'records')
         strata.balance(step())
-
     }, function () {
-
         objectify(tmp, step())
         load(__dirname + '/fixtures/tree.after.json', step())
-
     }, function (actual, expected) {
-
         deepEqual(actual, expected, 'merge')
-//    console.log(require('util').inspect(actual, false, null))
-//    console.log(require('util').inspect(expected, false, null))
-
         strata.close(step())
-
     })
 })
