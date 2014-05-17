13ce1596a585b20a357ced79afbc53c2a7d87fef
Alan Gutierrez <alan@prettyrobots.com>
2014-05-16 21:44:15 -0700

    Tidy `t/lock/purge-skip.t.js`.
    See #351.

--- t/lock/purge-skip.t.js
+++ t/lock/purge-skip.t.js
@@ -1,32 +1,21 @@
 #!/usr/bin/env node
 
-require('./proof')(2, function (step, equal, Strata, tmp, serialize) {
+require('./proof')(2, function (step, Strata, tmp, serialize, equal) {
     var strata
-
     step(function () {
-
         serialize(__dirname + '/fixtures/tree.before.json', tmp, step())
-
     }, function () {
-
         strata = new Strata({ directory: tmp, leafSize: 3, branchSize: 3 })
         strata.open(step())
-
     }, function () {
-
         strata.mutator('h', step())
-
     }, function (cursor) {
-
         equal(strata.size, 41, 'before purge')
         strata.purge(0)
         equal(strata.size, 18, 'after purge')
         cursor.unlock()
-
     }, function() {
-
         strata.close(step())
-
     })
 })
 
