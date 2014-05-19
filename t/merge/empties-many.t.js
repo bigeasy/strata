3212ab35f605bd54aadc8eebe499898649f175e5
Alan Gutierrez <alan@prettyrobots.com>
2014-05-19 11:07:59 -0700

    Use `vivify` in `t/merge/empties-many.t.js`.
    See #352.

--- t/merge/empties-many.t.js
+++ t/merge/empties-many.t.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-require('./proof')(1, function (step, Strata, tmp, load, serialize, objectify, assert) {
+require('./proof')(1, function (step, Strata, tmp, load, serialize, vivify, assert) {
     var strata = new Strata({ directory: tmp, leafSize: 3, branchSize: 3 })
     step(function () {
         serialize(__dirname + '/fixtures/empties-many.before.json', tmp, step())
@@ -17,7 +17,7 @@ require('./proof')(1, function (step, Strata, tmp, load, serialize, objectify, a
     }, function () {
         strata.balance(step())
     }, function () {
-        objectify(tmp, step())
+        vivify(tmp, step())
         load(__dirname + '/fixtures/empties-many.after.json', step())
     }, function (actual, expected) {
         assert(actual, expected, 'after')
