db5c4e5f099bc528d4b40d58a4663d056149aebd
Alan Gutierrez <alan@prettyrobots.com>
2014-05-21 21:40:07 -0700

    Use `vivify` in `t/merge/empties-pivot.t.js`.
    See #352.

--- t/merge/empties-pivot.t.js
+++ t/merge/empties-pivot.t.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-require('./proof')(1, function (step, Strata, tmp, load, serialize, objectify, assert) {
+require('./proof')(1, function (step, Strata, tmp, load, serialize, vivify, assert) {
     var strata = new Strata({ directory: tmp, leafSize: 3, branchSize: 3 })
     step(function () {
         serialize(__dirname + '/fixtures/empties-pivot.before.json', tmp, step())
@@ -19,7 +19,7 @@ require('./proof')(1, function (step, Strata, tmp, load, serialize, objectify, a
         strata.balance(step())
     }, function () {
         console.log('after balance')
-        objectify(tmp, step())
+        vivify(tmp, step())
         load(__dirname + '/fixtures/empties-pivot.after.json', step())
     }, function (actual, expected) {
         assert(actual, expected, 'after')
