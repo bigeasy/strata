41099ec295bddd1ce183c777ea6ad2ed48094fa9
Alan Gutierrez <alan@prettyrobots.com>
2014-05-26 22:17:54 -0700

    Tidy `t/merge/left-singles.t.js`.
    See #351.

--- t/merge/left-singles.t.js
+++ t/merge/left-singles.t.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-require('./proof')(1, function (step, Strata, tmp, deepEqual, serialize, gather, load, objectify, say) {
-    var strata = new Strata({ directory: tmp, leafSize: 3, branchSize: 3 }), fs = require('fs')
+require('./proof')(1, function (step, Strata, tmp, load, serialize, objectify, gather, say, deepEqual) {
+    var strata = new Strata({ directory: tmp, leafSize: 3, branchSize: 3 })
     step(function () {
         serialize(__dirname + '/fixtures/left-singles.before.json', tmp, step())
     }, function () {
