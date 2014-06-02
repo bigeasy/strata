0a2c9793edca55f6b32b1926bc5362b4c89c1402
Alan Gutierrez <alan@prettyrobots.com>
2014-06-01 23:37:22 -0700

    Use `vivify` in `t/merge/unlocker.t.js`.
    See #352.

--- t/merge/unlocker.t.js
+++ t/merge/unlocker.t.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-require('./proof')(1, function (step, Strata, tmp, load, serialize, objectify, gather, assert) {
+require('./proof')(1, function (step, Strata, tmp, load, serialize, vivify, gather, assert) {
     var strata = new Strata({ directory: tmp, leafSize: 3, branchSize: 3 })
     step(function () {
         serialize(__dirname + '/fixtures/unlocker.before.json', tmp, step())
@@ -17,7 +17,7 @@ require('./proof')(1, function (step, Strata, tmp, load, serialize, objectify, g
     }, function () {
         strata.balance(step())
     }, function () {
-        objectify(tmp, step())
+        vivify(tmp, step())
         load(__dirname + '/fixtures/unlocker.after.json', step())
     }, function (actual, expected) {
         assert(actual, expected, 'balanced')
