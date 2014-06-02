746d5fd51102a2d4986397be57b982e317ba9890
Alan Gutierrez <alan@prettyrobots.com>
2014-06-01 23:42:07 -0700

    Use `assert` in `t/split/branch.t.js`.
    See #351.

--- t/split/branch.t.js
+++ t/split/branch.t.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-require('./proof')(4, function (step, Strata, tmp, load, serialize, objectify, gather, deepEqual) {
+require('./proof')(4, function (step, Strata, tmp, load, serialize, objectify, gather, assert) {
     var strata = new Strata({ directory: tmp, leafSize: 3, branchSize: 3 })
     step(function () {
         serialize(__dirname + '/fixtures/branch.before.json', tmp, step())
@@ -17,15 +17,15 @@ require('./proof')(4, function (step, Strata, tmp, load, serialize, objectify, g
     }, function () {
         gather(step, strata)
     }, function (records) {
-        deepEqual(records, [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n' ], 'records')
+        assert(records, [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n' ], 'records')
         strata.balance(step())
     }, function () {
         objectify(tmp, step())
         load(__dirname + '/fixtures/branch.after.json', step())
     }, function (actual, expected) {
-        deepEqual(actual, expected, 'split')
+        assert(actual, expected, 'split')
         strata.purge(0)
-        deepEqual(strata.size, 0, 'purge completely')
+        assert(strata.size, 0, 'purge completely')
         strata.close(step())
     }, function () {
         strata = new Strata({ directory: tmp, leafSize: 3, branchSize: 3 })
@@ -33,7 +33,7 @@ require('./proof')(4, function (step, Strata, tmp, load, serialize, objectify, g
     }, function () {
         gather(step, strata)
     }, function (records) {
-        deepEqual(records, [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n' ], 'records')
+        assert(records, [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n' ], 'records')
         strata.close(step())
     })
 })
