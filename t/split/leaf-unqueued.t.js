7d6dec7e868dcea8228f0bb77ab07c41dc6ebff9
Alan Gutierrez <alan@prettyrobots.com>
2014-06-02 23:48:33 -0700

    Use `vivify` in `t/split/leaf-unqueued.t.js`.
    See #352.

--- t/split/leaf-unqueued.t.js
+++ t/split/leaf-unqueued.t.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-require('./proof')(3, function (step, Strata, tmp, load, serialize, objectify, gather, assert) {
+require('./proof')(3, function (step, Strata, tmp, load, serialize, vivify, gather, assert) {
     var strata = new Strata({ directory: tmp, leafSize: 3, branchSize: 3 })
     step(function () {
         serialize(__dirname + '/fixtures/leaf-remainder.before.json', tmp, step())
@@ -16,7 +16,7 @@ require('./proof')(3, function (step, Strata, tmp, load, serialize, objectify, g
     }, function (records) {
         assert(records, [ 'a', 'c', 'd', 'e', 'f', 'g', 'h' ], 'records after balance')
 
-        objectify(tmp, step())
+        vivify(tmp, step())
         load(__dirname + '/fixtures/leaf-remainder.before.json', step())
     }, function (actual, expected) {
         assert(actual, expected, 'split')
