1d16fe8a58c775c9125c545f1e46664051e95cae
Alan Gutierrez <alan@prettyrobots.com>
2014-06-02 23:48:08 -0700

    Use `assert` in `t/split/leaf-unqueued.t.js`.
    See #351.

--- t/split/leaf-unqueued.t.js
+++ t/split/leaf-unqueued.t.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-require('./proof')(3, function (step, Strata, tmp, load, serialize, objectify, gather, deepEqual) {
+require('./proof')(3, function (step, Strata, tmp, load, serialize, objectify, gather, assert) {
     var strata = new Strata({ directory: tmp, leafSize: 3, branchSize: 3 })
     step(function () {
         serialize(__dirname + '/fixtures/leaf-remainder.before.json', tmp, step())
@@ -9,17 +9,17 @@ require('./proof')(3, function (step, Strata, tmp, load, serialize, objectify, g
     }, function () {
         gather(step, strata)
     }, function (records) {
-        deepEqual(records, [ 'a', 'c', 'd', 'e', 'f', 'g', 'h' ], 'records')
+        assert(records, [ 'a', 'c', 'd', 'e', 'f', 'g', 'h' ], 'records')
         strata.balance(step())
     }, function () {
         gather(step, strata)
     }, function (records) {
-        deepEqual(records, [ 'a', 'c', 'd', 'e', 'f', 'g', 'h' ], 'records after balance')
+        assert(records, [ 'a', 'c', 'd', 'e', 'f', 'g', 'h' ], 'records after balance')
 
         objectify(tmp, step())
         load(__dirname + '/fixtures/leaf-remainder.before.json', step())
     }, function (actual, expected) {
-        deepEqual(actual, expected, 'split')
+        assert(actual, expected, 'split')
     }, function() {
         strata.close(step())
     })
