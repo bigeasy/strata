6d1ade471bc1f7844baa21a470334014716fdced
Alan Gutierrez <alan@prettyrobots.com>
2015-01-29 01:48:12 -0600

    Create a simplified file writer.
    See #426.

---
+++ scribe.js
@@ -0,0 +1,78 @@
+var fs = require('fs')
+
+function Scribe (filename, flags) {
+    this.operations = [ {} ]
+    fs.open(filename, flags, this._done.bind(this, '_opened'))
+}
+
+Scribe.prototype._done = function (method, error, result) {
+    if (error) {
+        while (this.operations.length) {
+            var callback = this.operations.shift().callback
+            if (callback) {
+                callback(error)
+                return
+            }
+        }
+        this.error = error
+    } else {
+        this[method](result)
+        this.operations.shift()
+        if (this.operations.length) {
+            this[this.operations[0].method](this.operations[0])
+        }
+    }
+}
+
+Scribe.prototype._opened = function (fd) {
+    this.fd = fd
+}
+
+Scribe.prototype.write = function (buffer, offset, length, position, callback) {
+    this._push({
+        method: '_write',
+        buffer: buffer,
+        offset: offset,
+        length: length,
+        position: position,
+        callback: callback
+    })
+}
+
+Scribe.prototype._push = function (operation) {
+    if (this.error) {
+        var callback = operation.callback
+        if (callback) {
+            var error = this.error
+            delete this.error
+            callback(error)
+        }
+    } else {
+        this.operations.push(operation)
+        if (this.operations.length == 1) {
+            this[this.operations[0].method](this.operations[0])
+        }
+    }
+}
+
+Scribe.prototype.close = function (callback) {
+    this._push({ method: '_close', callback: callback })
+}
+
+Scribe.prototype._write = function (write) {
+    fs.write(this.fd, write.buffer, write.offset, write.length,
+        write.position, this._done.bind(this, '_written'))
+}
+
+Scribe.prototype._written = function (written, buffer) {
+    var callback = this.operations[0].callback
+    if (callback) {
+        callback(null, written, buffer)
+    }
+}
+
+Scribe.prototype._close = function (write) {
+    fs.close(this.fd, write.callback)
+}
+
+module.exports = Scribe
---
+++ t/scribe/scribe.t.js
@@ -0,0 +1,59 @@
+require('proof')(3, require('cadence')(prove))
+
+function prove (async, assert) {
+    var path = require('path')
+    var rimraf = require('rimraf')
+    var mkdirp = require('mkdirp')
+    var fs = require('fs')
+    var Scribe = require('../../scribe')
+
+    var directory = path.join(__dirname, 'tmp')
+    var file = path.join(directory, 'file')
+    async(function () {
+        rimraf(directory, async())
+    }, function () {
+        mkdirp(directory, async())
+    }, function () {
+        var scribe = new Scribe(file, 'w+')
+        async(function () {
+            scribe.write(new Buffer('x'), 0, 1, 0)
+            scribe.write(new Buffer('y'), 0, 1, 1, async())
+        }, function () {
+            setImmediate(async())
+        }, function () {
+            scribe.write(new Buffer('z'), 0, 1, 2)
+            scribe.close(async())
+        })
+    }, function () {
+        fs.readFile(file, 'utf8', async())
+    }, function (body) {
+        assert(body, 'xyz', 'write')
+        async([function () {
+            var scribe = new Scribe(path.join(__dirname, 'missing', 'file'), 'w+')
+            scribe.write(new Buffer('x'), 0, 1, 0)
+            scribe.close(async())
+        }, function (_, error) {
+            assert(error.code, 'ENOENT', 'immediate error')
+        }])
+    }, function () {
+        fs.open(file, 'r', async())
+    }, function (fd) {
+        async([function () {
+            var scribe = new Scribe(path.join(directory, 'write'), 'w+')
+            async(function () {
+                scribe.write(new Buffer('y'), 0, 1, 0, async())
+            }, function () {
+                scribe.fd = fd
+                scribe.write(new Buffer('y'), 0, 1, 1)
+                setTimeout(async(), 500)
+            }, function () {
+                scribe.write(new Buffer('z'), 0, 1, 2)
+                scribe.close(async())
+            })
+        }, function (_, error) {
+            assert(error.code, 'EBADF', 'later error')
+        }])
+    }, function () {
+        rimraf(directory, async())
+    })
+}
