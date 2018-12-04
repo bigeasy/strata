bd0343f870f0196fa03ed0f6bbd2f205967e50fa
Alan Gutierrez <alan@prettyrobots.com>
2018-12-03 20:00:43 -0600

    Restore test by test.
    Yes, let's preserve all tests and restore functionality test by test.
    Move insert testing into the insert test.

--- t/basics/insert.t.js
+++ t/basics/insert.t.js
@@ -1,32 +1,56 @@
 require('./proof')(4, prove)
 
 function prove (async, okay) {
-    var strata
+    var Strata = require('../..')
+
+    var rimraf = require('rimraf')
+    var mkdirp = require('mkdirp')
+    var path = require('path')
+    var directory = path.resolve(__dirname, '../tmp')
+
+    var utilities = require('../utilities')
+
+    var options = {
+        directory: directory,
+        branch: { split: 5, merge: 2 },
+        leaf: { split: 5, merge: 2 }
+    }
+    var strata = new Strata(options)
+
     async(function () {
-        strata = createStrata({ directory: tmp, leafSize: 3, branchSize: 3 })
-        strata.create(async())
+        rimraf(directory, async())
     }, function () {
-        strata.mutator('a', async())
-    }, function (cursor) {
-        okay(cursor.exclusive, 'exclusive')
-        cursor.insert('a', 'a', ~cursor.index)
-        cursor.unlock(async())
+        mkdirp(directory, async())
     }, function () {
-        okay(strata.sheaf.magazine.heft, 54, 'json size')
-    }, function () {
-        vivify(tmp, async())
-        load(__dirname + '/fixtures/insert.json', async())
-    }, function (actual, expected) {
-        okay.say(expected)
-        okay.say(actual)
-
-        okay(actual, expected, 'insert')
-
-        okay.say(expected.segment00000001)
-
-        strata.purge(0)
-        okay(strata.sheaf.magazine.heft, 0, 'purged')
-
-        strata.close(async())
+        var strata = new Strata(options)
+        async(function () {
+            strata.create(async())
+        }, function () {
+            strata.cursor('x', async())
+        }, function (cursor) {
+            async([function () {
+                cursor.close()
+            }], function () {
+                okay({
+                    sought: cursor.sought,
+                    found: cursor.found
+                }, {
+                    sought: 'x',
+                    found: cursor.found
+                }, 'cursor not found')
+                cursor.insert('x', 'x', cursor.index)
+                okay(cursor.indexOf('x', cursor.ghosts), 0, 'inserted')
+                okay(~cursor.indexOf('z', 0), 1, 'not found')
+                // TODO Assert heft and purge again.
+                okay(strata._sheaf.heft, 3, 'heft')
+            })
+        }, function () {
+            strata.close(async())
+        }, function () {
+            utilities.vivify(directory, async())
+        }, function (x) {
+            return
+            okay(x, {}, /*require('./fixtures/inserted'), */'inserted')
+        })
     })
 }
