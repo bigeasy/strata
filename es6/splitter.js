df0795792105abf88f464821e69ea1b31897250e
Alan Gutierrez <alan@prettyrobots.com>
2019-05-07 20:15:51 -0500

    Convert Splitter to ES6.

---
+++ es6/splitter.js
@@ -0,0 +1,65 @@
+const assert = require('assert')
+const EOL = Buffer.from('\n')
+
+class Splitter {
+    constructor (checksum) {
+        this._remainder = Buffer.alloc(0)
+        this._checksum = checksum
+        this._entry = { checksums: null, header: null, body: null, sizes: [] }
+    }
+
+    split (chunk) {
+        const buffer = Buffer.concat([ this._remainder, chunk ])
+        const buffers = []
+        let begin = 0
+        for (let i = 0, I = buffer.length; i < I; i++) {
+            if (buffer[i] == 0xa) {
+                buffers.push(buffer.slice(begin, i))
+                begin = i + 1
+            }
+        }
+        this._remainder = buffer.slice(begin)
+        const entries = [], checksum = this._checksum
+        buffers.forEach((buffer) => {
+            if (this._entry.checksums == null) {
+                this._entry.checksums = JSON.parse(buffer.toString())
+            } else if (this._entry.header == null) {
+                assert(checksum(buffer, 0, buffer.length) == this._entry.checksums[0])
+                this._entry.header = JSON.parse(buffer.toString())
+                this._entry.sizes.push(buffer.length)
+                if (this._entry.header.length == 0) {
+                    entries.push(this._entry)
+                    this._entry = { checksums: null, header: null, body: null, sizes: [] }
+                } else {
+                    this._entry.body = []
+                }
+            } else {
+                this._entry.body.push(buffer)
+                const length = this._entry.body.reduce(function (length, buffer) {
+                    return length + buffer.length
+                }, 0) + 1
+                if (length < this._entry.header.length) {
+                    this._entry.body.push(EOL)
+                } else {
+                    const body = Buffer.concat(this._entry.body)
+                    assert(checksum(body, 0, body.length) == this._entry.checksums[1])
+                    if (this._entry.header.json) {
+                        this._entry.body = JSON.parse(body.toString())
+                    } else {
+                        this._entry.body = body
+                    }
+                    this._entry.sizes.push(body.length)
+                    entries.push(this._entry)
+                    this._entry = { checksums: null, header: null, body: null, sizes: [] }
+                }
+            }
+        })
+        return entries
+    }
+
+    empty () {
+        return this._remainder.length == 0
+    }
+}
+
+module.exports = Splitter
---
+++ es6/test/splitter.t.js
@@ -0,0 +1,41 @@
+describe('splitter', () => {
+    function checksum (buffer, start, end) { return String(end - start) }
+    const assert = require('assert')
+    const recorder = require('../../recorder')(checksum)
+    const Splitter = require('../splitter')
+    it('can be constructed', () => {
+        const splitter = new Splitter(checksum)
+        assert(splitter != null, 'constructed')
+    })
+    it('can split lines', () => {
+        var splitter = new Splitter(checksum)
+        const buffers = [
+            recorder({ value: 1 }),
+            recorder({}, { value: 1 }),
+            recorder({}, Buffer.from('abcdefghijklm\nnopqrstuvwxyz'))
+        ]
+        const buffer = Buffer.concat(buffers)
+        assert.deepStrictEqual(splitter.split(buffer.slice(0, 5)), [], 'partial')
+        assert(!splitter.empty(), 'splitter has remainder')
+        assert.deepStrictEqual(splitter.split(buffer.slice(5, 120)), [{
+            checksums: [ '22' ],
+            header: { value: 1, length: 0 },
+            body: null,
+            sizes: [ 22 ]
+        }, {
+            checksums: [ '25', '11' ],
+            header: { json: true, length: 12 },
+            body: { value: 1 },
+            sizes: [ 25, 11 ]
+        }], 'body partial')
+        assert.deepStrictEqual(splitter.split(buffer.slice(120)).map(function (entry) {
+            entry.body = entry.body.toString().split('\n')
+            return entry
+        }), [{
+            checksums: [ '13', '27' ],
+            header: { length: 28 },
+            body: [ 'abcdefghijklm', 'nopqrstuvwxyz' ],
+            sizes: [ 13, 27 ]
+        }], 'body remainder')
+    })
+})
