7ac9fb7abfbec5c2a2a82714d2327c90abd78942
Alan Gutierrez <alan@prettyrobots.com>
2020-08-15 11:15:01 -0500

    Update for a bug in Destructible.
    There was a bug where ephemeral's where shutting down because their
    durables where always exiting with an error flag, or the error flag of
    the ephemeral would always be set to true instead of the error status of
    the durable child.

--- test/split.t.js
+++ test/split.t.js
@@ -1,6 +1,5 @@
 require('proof')(2, async (okay) => {
     const Destructible = require('destructible')
-    const destructible = new Destructible('split.t')
 
     const Strata = require('../strata')
     const Cache = require('../cache')
@@ -26,51 +25,49 @@ require('proof')(2, async (okay) => {
         ]]
     })
 
-    destructible.durable('test', async function () {
-        async function split () {
-            const cache = new Cache
-            const strata = new Strata(destructible.ephemeral('split'), { directory, cache })
-            await strata.open()
-            const cursor = (await strata.search('f')).get()
-            cursor.insert('f', 'f', cursor.index)
+    await async function () {
+        const cache = new Cache
+        const destructible = new Destructible([ 'split.t', 'split' ])
+        const strata = new Strata(destructible, { directory, cache })
+        await strata.open()
+        const cursor = (await strata.search('f')).get()
+        cursor.insert('f', 'f', cursor.index)
+        cursor.release()
+        await cursor.flush()
+        await strata.close()
+        cache.purge(0)
+        // **TODO** broken test...
+        // okay(cache.heft, 0, 'cache purged')
+        await destructible.destructed
+    } ()
+    await async function () {
+        const destructible = new Destructible([ 'split.t', 'reopen' ])
+        const cache = new Cache
+        const strata = new Strata(destructible, { directory, cache })
+        await strata.open()
+        const cursor = (await strata.search('f')).get()
+        okay(cursor.items[cursor.index].value, 'f', 'found')
+        cursor.release()
+        await strata.close()
+        await destructible.destructed
+    } ()
+    await async function () {
+        const destructible = new Destructible([ 'split.t', 'traverse' ])
+        const cache = new Cache
+        const strata = new Strata(destructible, { directory, cache })
+        await strata.open()
+        let right = 'a'
+        const items = []
+        do {
+            const cursor = (await strata.search(right)).get()
+            for (let i = cursor.index; i < cursor.page.items.length; i++) {
+                items.push(cursor.page.items[i].value)
+            }
             cursor.release()
-            await cursor.flush()
-            await strata.close()
-            cache.purge(0)
-            // **TODO** broken test...
-            // okay(cache.heft, 0, 'cache purged')
-        }
-        await split()
-        async function reopen () {
-            const cache = new Cache
-            const strata = new Strata(destructible.ephemeral('reopen'), { directory, cache })
-            await strata.open()
-            const cursor = (await strata.search('f')).get()
-            okay(cursor.items[cursor.index].value, 'f', 'found')
-            cursor.release()
-            await strata.close()
-        }
-        await reopen()
-        async function traverse () {
-            const cache = new Cache
-            const strata = new Strata(destructible.ephemeral('traverse'), { directory, cache })
-            await strata.open()
-            let right = 'a'
-            const items = []
-            do {
-                const cursor = (await strata.search(right)).get()
-                for (let i = cursor.index; i < cursor.page.items.length; i++) {
-                    items.push(cursor.page.items[i].value)
-                }
-                cursor.release()
-                right = cursor.page.right
-            } while (right != null)
-            okay(items, [ 'a', 'b', 'c', 'd', 'e', 'f' ], 'traverse')
-            await strata.close()
-        }
-        await traverse()
-        destructible.destroy()
-    })
-
-    await destructible.destructed
+            right = cursor.page.right
+        } while (right != null)
+        okay(items, [ 'a', 'b', 'c', 'd', 'e', 'f' ], 'traverse')
+        await strata.close()
+        await destructible.destructed
+    } ()
 })
