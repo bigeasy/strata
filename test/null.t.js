4c967be6e14c9aa794fc9d0f7c84a7f009fe86af
Alan Gutierrez <alan@prettyrobots.com>
2020-09-02 06:07:57 -0500

    Fix `nullCursor` test.
    See #625.

--- test/null.t.js
+++ test/null.t.js
@@ -1,6 +1,8 @@
 require('proof')(1, okay => {
     const Strata = require('..')
     const cursor = Strata.nullCursor()
-    okay(cursor.indexOf(cursor.page.ghosts, 'a'), null, 'null')
+    okay(cursor.indexOf(cursor.page.ghosts, 'a'), {
+        index: null, found: false
+    }, 'null')
     cursor.release()
 })
