f2d0e2108c37eca2f09a54eeb82a055417ca82c7
Alan Gutierrez <alan@prettyrobots.com>
2020-11-13 23:43:44 -0600

    Test housekeeping race.
    We have a race where we add a page to housekeeping for split or merge
    but the split or merge condition is no longer valid when housekeeping
    begins. We test it here by simply reaching split then unreaching split
    hopefully before the housekeeping queue gets to work on the entry. Our
    hopes are realized with this test, but it may or may not be
    deterministic. It may be deterministic, based on the order of entries in
    the PromiseJobs queue, or it might be non-deterministic, based on the
    vagaries of my workstation. Time will tell. Better coverage than before.
    
    Closes #734.

---
+++ test/housekeeping.t.js
@@ -0,0 +1,43 @@
+// This should test a race where a page is queued for housekeeping because it
+// has reached the split boundary but when housekeeping queue begins work the
+// page has shrunk and is no longer ready to split.
+
+//
+require('proof')(1, async (okay) => {
+    const Trampoline = require('reciprocate')
+    const Destructible = require('destructible')
+
+    const Strata = require('../strata')
+    const Cache = require('../cache')
+
+    const utilities = require('../utilities')
+    const path = require('path')
+    const directory = path.join(utilities.directory, 'split')
+    await utilities.reset(directory)
+    await utilities.serialize(directory, {
+        '0.0': [[ '0.1', null ]],
+        '0.1': [[ 'insert', 0, 'a' ], [ 'insert', 1, 'b' ], [ 'insert', 2, 'c' ], [ 'insert', 3, 'd' ]]
+    })
+
+    {
+        const destructible = new Destructible([ 'load.t' ])
+        const strata = await Strata.open(destructible, { directory, cache: new Cache })
+        const items = []
+        const trampoline = new Trampoline
+        strata.search(trampoline, 'e', cursor => {
+            cursor.insert(cursor.index, 'e', [ 'e' ], {})
+        })
+        while (trampoline.seek()) {
+            await trampoline.shift()
+        }
+        await null
+        strata.search(trampoline, 'e', cursor => {
+            cursor.remove(cursor.index, {})
+        })
+        while (trampoline.seek()) {
+            await trampoline.shift()
+        }
+        okay('true')
+        await strata.destructible.destroy().rejected
+    }
+})
